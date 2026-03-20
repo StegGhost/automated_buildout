aa223ef01de380e31e124e44fad711fba9b43c89
StegVerse <rigelrandolph@gmail.com>
2026-03-20 14:02:10 -0500

    Update run_buildout.py

--- run_buildout.py
+++ run_buildout.py
@@ -1,11 +1,11 @@
 import json
 from engine.runner import run_build
 
-TARGET = "demo_target"
 
 def main():
-    result = run_build(TARGET)
+    result = run_build()
     print(json.dumps(result, indent=2))
+
 
 if __name__ == "__main__":
     main()
