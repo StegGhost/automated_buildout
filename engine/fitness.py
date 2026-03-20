fac8f03377b1c0702644b8fd7081b5d402857496
StegVerse <rigelrandolph@gmail.com>
2026-03-20 15:44:18 -0500

    Create fitness.py

---
+++ engine/fitness.py
@@ -0,0 +1,10 @@
+def score_phase(validation_result: dict, consensus_receipt: dict = None) -> float:
+    score = 0.0
+
+    if validation_result.get("valid"):
+        score += 0.5
+
+    if consensus_receipt:
+        score += consensus_receipt.get("agreement_ratio", 0) * 0.5
+
+    return score
